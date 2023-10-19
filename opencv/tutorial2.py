976988c81c0639bf52f5dcc0d3e31029d84f5f4a
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-19 22:50:35 +0800

    update

--- opencv/tutorial2.py
+++ opencv/tutorial2.py
@@ -1,21 +1,20 @@
 import cv2
 import numpy as np
 
-kernel = np.ones((3,3),np.uint8)
+# B G R
 
-img = cv2.imread('test1.jpg')
-img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
+img = cv2.imread('colorcolor.jpg')
+# print(img.shape)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(img,(7,7),0)
-canny = cv2.Canny(img,100,150)
-dilate = cv2.dilate(canny,kernel,iterations=1)
-erode = cv2.erode(dilate,kernel,iterations=1)
+# 製作綠色圖片
+# img = np.empty((300,300,3),np.uint8)
+
+# for row in range(300):
+#     for col in range(img.shape[1]):
+#         img[row, col] = [0,255,0]
+
+newImg = img[:150,:200]
 
 cv2.imshow('img',img)
-cv2.imshow('gray',gray)
-cv2.imshow('blur',blur)
-cv2.imshow('canny',canny)
-cv2.imshow('dilate',dilate)
-cv2.imshow('erode',erode)
+cv2.imshow('newImg',newImg)
 cv2.waitKey(0)
